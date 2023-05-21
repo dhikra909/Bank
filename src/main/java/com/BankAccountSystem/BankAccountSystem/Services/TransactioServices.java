a3d63d585970665b76ec71e288a147b9f91cc7d5
dhikra909 <alm@ka909@gmail.com>
2023-05-21 16:41:52 +0400

    9

--- src/main/java/com/BankAccountSystem/BankAccountSystem/Services/TransactioServices.java
+++ src/main/java/com/BankAccountSystem/BankAccountSystem/Services/TransactioServices.java
@@ -42,7 +42,9 @@ public class TransactioServices {
         transactioRepositores.getTransactionById(id);
     }
 
-    
+    public void deleteAllTransaction() {
+        transactioRepositores.deleteAllTransaction();
+    }
 
 
 
